e2fd16c1af1818eaa10463122480233fb9c98f36
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-07-01 14:31:23 +0900

    CORS Error Fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { connectToDatabase } from "./database.js";
 
 import { signup, login } from "./auth.js";
@@ -6,6 +7,13 @@ import { signup, login } from "./auth.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "DELETE", "UPDATE"],
+  allowHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 connectToDatabase();
 
