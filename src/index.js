d7cdafa0ae10e2b501d8ffd0f272d1dd3e86af88
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-07-18 00:29:37 +0900

    회원 정보 수정 api

--- src/index.js
+++ src/index.js
@@ -42,6 +42,59 @@ app.get("/users", async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+// 회원 정보 수정
+app.put("/users/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const {
+    userType,
+    userId,
+    password,
+    name,
+    nickname,
+    email,
+    school,
+    studentId,
+    positions,
+    backNumber,
+  } = res.body;
+
+  if (
+    !userType &&
+    !userId &&
+    !name &&
+    !email &&
+    !password &&
+    !nickname &&
+    !school &&
+    !studentId &&
+    !positions &&
+    !backNumber
+  ) {
+    return res.status(400).send("하나의 항목이라도 변경되어야 합니다.");
+  }
+  try {
+    const updateData = {};
+    if (userType) updateData.userType = userType;
+    if (userId) updateData.userId = userId;
+    if (password) updateData.password = password;
+    if (name) updateData.name = name;
+    if (nickname) updateData.nickname = nickname;
+    if (email) updateData.email = email;
+    if (school) updateData.school = school;
+    if (studentId) updateData.studentId = studentId;
+    if (positions) updateData.positions = positions;
+    if (backNumber) updateData.backNumber = backNumber;
+    const updateUser = await User.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+    if (!updateUser) {
+      return res.status(404).send("사용자를 찾을 수 없습니다.");
+    }
+    res.json(updateUser);
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
